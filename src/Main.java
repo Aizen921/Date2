5d71cd7bed502f55e1bb490c8a858a73aa5d01d0
gianluca <alex992palermo@gmail.com>
2023-12-16 20:42:09 +0100

    Aggiunto Esercizio Date2

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+        OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        String nuovaData = data.format(DateTimeFormatter.ofPattern("dd MMMM yyyy"));
+
+        System.out.println("Nuova Data: " + nuovaData);
+    }
+}
